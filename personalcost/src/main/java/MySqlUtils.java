bbdd5a747cf8528beb8b1a07820ecdc78eb174d4
Delevin <502202587@qq.com>
2022-11-02 16:22:51 +0800

    Add maven assembly plugin Add slf4j-simple Comment the LOG function in MySqlUtils.java

--- personalcost/src/main/java/MySqlUtils.java
+++ personalcost/src/main/java/MySqlUtils.java
@@ -14,16 +14,16 @@ public class MySqlUtils {
 
     public static void insertNewRecord(Connection conn, String currentDate, String costItem, String cost,
                                        String costType, String tableName) throws SQLException {
-        LOG.info("Insert new records");
+//        LOG.info("Insert new records");
         String insertQuery = String.format("INSERT INTO %s VALUES ('%s', '%s', '%s', '%s');", tableName,
                 costItem, cost, costType, currentDate);
-        LOG.info("Executing query : {}", insertQuery);
+//        LOG.info("Executing query : {}", insertQuery);
         Statement statement = conn.createStatement();
         statement.execute(insertQuery);
     }
 
     public static void coreateCostTable(Connection conn, String tableName) throws SQLException {
-        LOG.info("Creating {} table", tableName);
+//        LOG.info("Creating {} table", tableName);
         String createStmt = String.format("CREATE TABLE IF NOT EXISTS `%s` (  \n" +
                 "  `costItem` varchar(20) NOT NULL,  \n" +
                 "  `cost` decimal(10,2) NOT NULL,  \n" +
@@ -32,13 +32,13 @@ public class MySqlUtils {
                 "  PRIMARY KEY (`costItem`, `cost`)  \n" +
                 ") ENGINE=InnoDB DEFAULT CHARSET=utf8;", tableName);
 
-        LOG.info("Execute Query : \n {}", createStmt);
+//        LOG.info("Execute Query : \n {}", createStmt);
         Statement statement = conn.createStatement();
         statement.execute(createStmt);
     }
 
     public static ResultSet queryCostTable(Connection connection, String query) throws SQLException {
-        LOG.info("Execute query {}", query);
+//        LOG.info("Execute query {}", query);
         Statement stmt = connection.createStatement();
         return stmt.executeQuery(query);
     }
